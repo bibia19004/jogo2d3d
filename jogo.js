0a8b8bcc1e02bbfcfcdcc148e82bc467c2e5e0a3
Beatriz Becker <beatrizbfeldman@gmail.com>
2025-03-05 09:19:41 -0300

    inicializando js

--- jogo.js
+++ jogo.js
@@ -1,2 +1,10 @@
-a=1
+//pegar o elemento canvas pelo id
+const canvas = document.getElementById9('jogo2D')
+//inicializar o canvas
+const ctx = canvas.getContext('2d')
 
+//criar a função loop
+function loop () {
+
+}
+loop()
