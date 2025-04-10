997b4b2872fd667add9eb34eb730e880b6531614
BEATRIZ BECKER FELDMAN <beatriz_feldman@estudante.sesisenai.org.br>
2025-04-10 09:38:24 -0300

    jogo completo

--- entidades.js
+++ entidades.js
@@ -1,29 +1,37 @@
-const canvas = document.getElementById('jogo2D')
-const ctx = canvas.getContext('2d')
-let gameOver = false
+const canvas = document.getElementById('jogo2D');
+const ctx = canvas.getContext('2d');
+let gameOver = false;
 
 const obstaculos = [];
 let frameCount = 0;
+let pontos = 0;
+let pontuacaoMaisAlta = 0;
+
+const backgroundImage = new Image();
+backgroundImage.src = 'https://acdn-us.mitiendanube.com/stores/963/751/products/casa-da-barbie-editadosite1-e466d9ce5e7e7edd8d16901530882090-1024-1024.png';
+
+const personagemImage = new Image();
+personagemImage.src = './image.png';
 
 document.addEventListener('keypress', (e) => {
-    if(e.code == 'Space' && personagem.pulando == false && !gameOver) {
-        console.log('clicou para pular')
-        personagem.saltar()
+    if (e.code == 'Space' && personagem.pulando == false && !gameOver) {
+        personagem.saltar();
+    } else if (e.code == 'Space' && gameOver) {
+        reiniciarJogo();
     }
-})
-
+});
 
 class Entidade {
-    #gravidade
-    constructor (x, y, largura, altura){
+    #gravidade;
+    constructor(x, y, largura, altura) {
         this.x = x;
         this.y = y;
         this._largura = largura;
         this._altura = altura;
-        this.#gravidade = 0.5
+        this.#gravidade = 1; // Aumenta a gravidade para cair mais rápido
     }
-    get gravidade(){
-        return this.#gravidade
+    get gravidade() {
+        return this.#gravidade;
     }
 
     get largura() {
@@ -34,63 +42,66 @@ class Entidade {
         return this._altura;
     }
 
-    desenhar = function (cor){
-        ctx.fillStyle = cor
-        ctx.fillRect(this.x, this.y, this.largura, this.altura)
+    desenhar(cor) {
+        ctx.fillStyle = cor;
+        ctx.fillRect(this.x, this.y, this.largura, this.altura);
     }
 }
 
-class Personagem extends Entidade{
-    #pulando
-    #velocidadey 
-    constructor(x, y, largura, altura){
-        super(x, y, largura, altura) 
-        this.#pulando= false
-        this.#velocidadey = 0
+class Personagem extends Entidade {
+    #pulando;
+    #velocidadey;
+    constructor(x, y, largura, altura) {
+        super(x, y, largura, altura);
+        this.#pulando = false;
+        this.#velocidadey = 0;
     }
-    saltar = function (){
-        this.#velocidadey = 15
-        this.#pulando = true
-        console.log('saltou')
+    saltar() {
+        this.#velocidadey = 20; // Aumenta a velocidade inicial do pulo
+        this.#pulando = true;
     }
-    get pulando () {
-        return this.#pulando
+    get pulando() {
+        return this.#pulando;
     }
-    set pulando (valor) {
-        this.#pulando = valor
+    set pulando(valor) {
+        this.#pulando = valor;
     }
-    get velocidadey () {
-        return this.#velocidadey
+    get velocidadey() {
+        return this.#velocidadey;
     }
-    set velocidadey (valor) {
-        this.#velocidadey = valor
+    set velocidadey(valor) {
+        this.#velocidadey = valor;
     }
-    atualizarPersonagem = function () {
+    atualizarPersonagem() {
         if (this.pulando) {
-            this.velocidadey -= this.gravidade
-            this.y -= this.velocidadey
-            if (this.y >= canvas.height-50) {
-                this.velocidadey = 0
-                this.pulando = false
-                this.y = canvas.height-50
+            this.velocidadey -= this.gravidade;
+            this.y -= this.velocidadey;
+            if (this.y >= canvas.height - 100) {
+                this.velocidadey = 0;
+                this.pulando = false;
+                this.y = canvas.height - 100;
             }
         }
     }
+    desenhar() {
+        ctx.drawImage(personagemImage, this.x, this.y, this.largura, this.altura);
+    }
 }
 
-class Obstaculo extends Entidade{
-    constructor (x, y, largura, altura){
-        super(x, y, largura, altura)
+class Obstaculo extends Entidade {
+    constructor(x, y, largura, altura) {
+        super(x, y, largura, altura);
     }
     atualizarObstaculo() {
         this.x -= 5;
     }
 }
 
-const personagem = new Personagem (100, canvas.height - 50, 50 ,50)
+const personagem = new Personagem(100, canvas.height - 100, 100, 100);
 
 function gerarObstaculo() {
-    const obstaculo = new Obstaculo(canvas.width, canvas.height - 50, 30, 50);
+    const altura = Math.random() * 50 + 20; // Altura aleatória
+    const obstaculo = new Obstaculo(canvas.width, canvas.height - altura, 30, altura);
     obstaculos.push(obstaculo);
 }
 
@@ -106,29 +117,57 @@ function detectarColisao(obstaculo) {
 function atualizarObstaculos() {
     for (let i = obstaculos.length - 1; i >= 0; i--) {
         obstaculos[i].atualizarObstaculo();
-        obstaculos[i].desenhar('Red');
+        obstaculos[i].desenhar('#FF1493');
 
         if (detectarColisao(obstaculos[i])) {
             gameOver = true;
-            alert('Game Over!');
+            ctx.fillStyle = 'pink';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = '#FF1493';
+            ctx.font = '48px serif';
+            ctx.textAlign = 'center';
+            ctx.fillText('Game Over!', canvas.width / 2, canvas.height / 2);
             return;
         }
 
         if (obstaculos[i].x + obstaculos[i].largura < 0) {
             obstaculos.splice(i, 1);
+            pontos++;
+            if (pontos > pontuacaoMaisAlta) {
+                pontuacaoMaisAlta = pontos;
+            }
         }
     }
 }
 
-function loop () {
+function reiniciarJogo() {
+    gameOver = false;
+    obstaculos.length = 0;
+    frameCount = 0;
+    pontos = 0;
+    personagem.x = 100;
+    personagem.y = canvas.height - 100;
+    personagem.velocidadey = 0;
+    personagem.pulando = false;
+    loop();
+}
+
+function loop() {
     if (!gameOver) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        personagem.desenhar('Black')
-        personagem.atualizarPersonagem()
+        ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+        personagem.desenhar();
+        personagem.atualizarPersonagem();
         atualizarObstaculos();
 
+        ctx.fillStyle = '#FF1493';
+        ctx.font = '24px serif';
+        ctx.textAlign = 'center'; // Centraliza o texto
+        ctx.fillText(`Pontuação: ${pontos}`, canvas.width / 2, 30); // Reposiciona a pontuação
+        ctx.fillText(`Pontuação Mais Alta: ${pontuacaoMaisAlta}`, canvas.width / 2, 60); // Reposiciona a pontuação mais alta
+
         frameCount++;
-        if (frameCount % 100 === 0) {
+        if (frameCount % 100 === 0) { // Diminui o intervalo para gerar mais obstáculos
             gerarObstaculo();
         }
 
